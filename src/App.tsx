6b832b48817552e13c7d0fb421500b889aed9088
mitani24 <h.mitani@ashita-team.com>
2025-07-19 02:12:34 +0900

    update heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="p-6">
+      <h1 className="scroll-m-20 text-4xl font-extrabold">
+        Notion Cover Generator
+      </h1>
     </div>
   );
 }
