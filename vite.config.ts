54790e1ff477692f185d060ace500f7bbb2c7b4d
mitani24 <h.mitani@ashita-team.com>
2025-07-19 12:03:14 +0900

    deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.NODE_ENV === "production" ? "/notion-cover-generator/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
